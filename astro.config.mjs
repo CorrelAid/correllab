e570582c7f0221cf867062b9dea6d89e2421eab6
fubits1 <ilja_s@protonmail.com>
2025-06-20 17:48:44 +0200

    build: test prefetch

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,7 @@ import devtoolsJson from "vite-plugin-devtools-json";
 
 // https://astro.build/config
 export default defineConfig({
+  prefetch: true,
   integrations: [svelte()],
   vite: {
     plugins: [devtoolsJson()],

f51739058b5c6a38dddce3bb4ae014be6b96ac2e
fubits1 <ilja_s@protonmail.com>
2025-06-23 23:09:28 +0200

    refactor: consolidate people

---
+++ src/data/people.js
@@ -0,0 +1,38 @@
+import ImageAnnKristin from "@assets/images/vester-annkristin.jpeg";
+import ImageZoe from "@assets/images/wolter-zoe.jpg";
+import ImageEmma from "@assets/images/morlock-emma.png";
+import ImageAntje from "@assets/images/relitz-antje.png";
+import ImageJonas from "@assets/images/lorenz-jonas.png";
+
+export const people = {
+  annkristin: {
+    name: "Ann-Kristin Vester",
+    role: "Geschäftsführung",
+    image: ImageAnnKristin,
+    email: "lab@correlaid.org?subject=CorrelLAB&body=Hi%20Ann-Kristin%2C",
+  },
+  zoe: {
+    name: "Zoé Wolter",
+    role: "Geschäftsführung und Bildung",
+    image: ImageZoe,
+    email: "lab@correlaid.org?subject=CorrelLAB&body=Hi%20Ann-Kristin%2C",
+  },
+  antje: {
+    name: "Antje Relitz",
+    role: "Bildung",
+    image: ImageAntje,
+    email: "lab@correlaid.org?subject=CorrelLAB&body=Hi%20Antje%2C",
+  },
+  emma: {
+    name: "Emma Morlock",
+    role: "Werkstudentin Data Literacy",
+    image: ImageEmma,
+    email: "lab@correlaid.org?subject=CorrelLAB&body=Hi%20Emma%2C",
+  },
+  jonas: {
+    name: "Jonas Lorenz",
+    role: "Werkstudent Bildung",
+    image: ImageJonas,
+    email: "lab@correlaid.org?subject=CorrelLAB&body=Hi%20Jonas%2C",
+  },
+};

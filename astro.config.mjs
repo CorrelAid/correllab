65f9394633f4cb2d409766377abaf5ed156ea7be
fubits1 <ilja_s@protonmail.com>
2025-06-23 15:26:44 +0200

    build: init sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,10 +4,21 @@ import { defineConfig } from "astro/config";
 import svelte from "@astrojs/svelte";
 import devtoolsJson from "vite-plugin-devtools-json";
 
+import sitemap from "@astrojs/sitemap";
+
 // https://astro.build/config
 export default defineConfig({
   prefetch: true,
-  integrations: [svelte()],
+  site: "https://lab.correlaid.org",
+  integrations: [
+    svelte(),
+    sitemap({
+      // skip legacy redirects
+      filter: (page) =>
+        page !== "https://lab.correlaid.org/kurse/kurs1/" &&
+        page !== "https://lab.correlaid.org/kurse/kurs2/",
+    }),
+  ],
   vite: {
     plugins: [devtoolsJson()],
   },
